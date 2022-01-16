cf68aebbc206b8f031782c67d1dff0bccf76c0b2
Dev2212 <developervaarun@gmail.com>
2022-01-16 23:20:55 +0530

    security issue

--- TaskApiTut/TaskApiTut/settings.py
+++ TaskApiTut/TaskApiTut/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-9ynurtk+#c*_46_4ampmx&t5+&@r1^5kwl$y1hl2l*4rp0gx*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ef88-27-54-172-66.ngrok.io']
+ALLOWED_HOSTS = [
+]
 
 
 # Application definition
